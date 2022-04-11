54f62710f221c833d3d9e171b64d4415b9bf0560
GhadaGhram <103033611+GhadaGhram@users.noreply.github.com>
2022-04-11 21:29:54 +0200

    Update Manga.java

--- Manga.java
+++ Manga.java
@@ -2,5 +2,7 @@ public class Manga extends Livre {
 
   public Manga(){
   }
-  public Manga(String titre,String auteur,double prix, int
-}
+  public Manga(String titre, String auteur, double prix, int nbrePages, boolean couleur) {
+	super(titre, auteur, prix, nbrePages);
+	}}
+  
