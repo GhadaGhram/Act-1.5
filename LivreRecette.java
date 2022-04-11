071b4e31970a59b18a6ceb76185ae6b898830be5
GhadaGhram <103033611+GhadaGhram@users.noreply.github.com>
2022-04-11 21:37:15 +0200

    Update LivreRecette.java

--- LivreRecette.java
+++ LivreRecette.java
@@ -1,11 +1,8 @@
-package Bibliothéque;
-
 import java.util.ArrayList;
-
 public class LivreRecette extends Livre {
 
-	ArrayList<Recette> recettes = new ArrayList<>();
-    public LivreRecette(String titre, String auteur, float prix, int nombreDePages) {
+ArrayList<Recette> recettes = new ArrayList<>();
+public LivreRecette(String titre, String auteur, float prix, int nombreDePages) {
         super(titre, auteur, prix, nombreDePages);
     }
 
