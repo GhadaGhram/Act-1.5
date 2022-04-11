63fa927f1c630b6c0a5227d93a2e113b7abbc8b1
GhadaGhram <103033611+GhadaGhram@users.noreply.github.com>
2022-04-11 21:44:34 +0200

    Update Recette.java

--- Recette.java
+++ Recette.java
@@ -1,38 +1,29 @@
-package Bibliothéque;
-
 import java.util.ArrayList;
-
 public class Recette {
-	
 public String nom;
 public String description;
-public int nvDifficulté;
+public int nvDifficultÃ©;
 
-ArrayList<String> astuces=new ArrayList<String>();
-ArrayList<String> etapes=new ArrayList<String>();
+ArrayList<String> astuce=new ArrayList<String>();
+ArrayList<String> etape=new ArrayList<String>();
 
 public Recette() {
 	}
 	
-public Recette(String nom, String description, int nvDifficulté) {
-	super();
+public Recette(String nom, String description, int nvDifficultÃ©) {
 	this.nom = nom;
 	this.description = description;
-	this.nvDifficulté = nvDifficulté;
+	this.nvDifficultÃ© = nvDifficultÃ©;
 	}
 
-public void addAstuces(String astuces) {
-astuces.add(astuces);
+public void addAstuce(String astuce) {
+astuce.add(astuce);
 }
 
-public void addEtapes(String etapes) {
-etapes.add(etapes);
+public void addEtape(String etape) {
+etape.add(etape);
 
-}
-
-
-	
-}
+}}
 
 
 
